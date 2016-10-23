a63b665e348e83b5749ab2f56fdf16c173caf2c2
Pranav-Rastogi <pranav_rastogi@outlook.com>
2016-10-23 23:14:19 +0530

    Complete Swap.c

--- 3/Swap/swap.c
+++ 3/Swap/swap.c
@@ -5,9 +5,15 @@ int main(void)
     int a, b;
 
     scanf("%d%d", &a, &b);
-    printf("Calling swapval...");
+
+    printf("Calling swapval...\n");
     swapval(a, b);
-    printf("Value after call by value ::\na = %d\tb = %d", a, b);
+    printf("Value after call by value ::\na = %d\tb = %d\n", a, b);
+
+
+    printf("\nCalling swapref...");
+    swapref(&a, &b);
+    printf("Value after call by reference ::\na = %d\tb = %d", a, b);
     return 0;
 }
 
@@ -17,9 +23,14 @@ void swapval(int a, int b)
     c = a;
     a = b;
     b = c;
+    printf("Value in swapval ::\na = %d\tb = %d\n", a, b);
 }
 
 void swapref(int *a, int *b)
 {
-
+    int c;
+    c = *a;
+    *a = *b;
+    *b = c;
+    printf("Value in swapref ::\na = %d\tb = %d\n", *a, *b);
 }
